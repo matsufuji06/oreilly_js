8b2d4acf52ce2ebffbf770eb68b38acf7fb90375
matsufuji06 <n.matsufuji06@gmail.com>
2021-11-30 19:40:12 +0900

    変数内に変数の値を入れた

--- chapter3/main.js
+++ chapter3/main.js
@@ -2,3 +2,8 @@ let currentTempC = 16;
 console.log(currentTempC);
 currentTempC = 22.5;
 console.log(currentTempC);
+
+let room1 = "会議室A";
+console.log(room1);
+let currentRoom = room1;
+console.log(currentRoom);
