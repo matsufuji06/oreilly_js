a3901ec6ce57ca733eda55450f539ebacbd072cf
matsufuji06 <n.matsufuji06@gmail.com>
2021-12-05 11:10:41 +0900

    オブジェクトの練習

--- chapter3/main.js
+++ chapter3/main.js
@@ -2,21 +2,21 @@ let currentTempC = 16;
 console.log(currentTempC);
 currentTempC = 22.5;
 console.log(currentTempC);
-
+// ------------------------------
 let room1 = "会議室A";
 console.log(room1);
 let currentRoom = room1;
 console.log(currentRoom);
-
+// ------------------------------
 let currentTemp = 19.5;
 // const message = "現在の温度は" + currentTemp + "°Cです"
 const message = `現在の温度は${currentTemp}です。\n` +
 "もう時期暖かくなるはずですから少々お待ちください。"
 console.log(message);
-
+// ------------------------------
 const multiline = "1行目\n" + "2行目\n" + "3行目\n";
 console.log(multiline);
-
+// ------------------------------
 
 const RED = Symbol();
 const BLUE = Symbol();
@@ -25,4 +25,18 @@ console.log(RED);
 console.log(ORANGE);
 console.log(RED === BLUE);
 console.log(RED === ORANGE);
-
+// ------------------------------
+const obj = {};
+obj.color = "黄色";
+console.log(obj.color);
+console.log(obj["color"]);
+// ------------------------------
+const sam1 = {
+  name: "Sam",
+  age: 4
+};
+console.log(sam1);
+sam1.greeting = function(){
+  return "hello!";
+};
+console.log(sam1.greeting());
