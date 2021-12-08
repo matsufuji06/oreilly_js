0b5462576e63218b7a4edc899d27de3c69f3074d
matsufuji06 <n.matsufuji06@gmail.com>
2021-12-08 22:16:27 +0900

    ヘルパー関数を二つとりあえず設定

--- chapter4/main.js
+++ chapter4/main.js
@@ -1,3 +1,13 @@
 let funds = 50; //開始時の条件
 
 while(funds > 1 && funds < 100) funds = funds + 2;
+
+// m以上、n以下のランダムな整数を返す。
+function rand(m, n) {
+  return m + Math.floor((n - m + 1)*Math.random());
+}
+
+// サイコロの目のどれかひとつを表す文字列をランダムに返す。
+function randFace() {
+  return ["crown", "anchor", "heart", "spade", "club", "diamond"] [rand(0,5)];
+}
