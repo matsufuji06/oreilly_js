62bed9e810ef5c04f44a70acc37936ae7cb6011d
matsufuji06 <n.matsufuji06@gmail.com>
2021-12-09 21:02:03 +0900

    賭け金(totalBet)をランダムに分けるのをdo...whileで表現

--- chapter4/main.js
+++ chapter4/main.js
@@ -12,12 +12,19 @@ function randFace() {
   return ["crown", "anchor", "heart", "spade", "club", "diamond"] [rand(0,5)];
 }
 
-const bet = {crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
+const bets = {crown: 0, anchor: 0, heart: 0, spade: 0, club: 0, diamond: 0};
 let totalBet = rand(1, funds);
 if(totalBet === 7) {
   totalBet = funds;
-  bet.heart = totalBet;
+  bets.heart = totalBet;
 } else {
   // 賭け金(totalBet)をランダムに分ける
+  let remaining = totalBet;
+  do {
+    let bet = rand(1, remaining);
+    let face = randFace();
+    bets[face] = bets[face] + bet;
+    remaining = remaining - bet;
+  } while(remaining > 0);
 }
 funds = funds - totalBet;
